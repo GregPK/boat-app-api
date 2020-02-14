a846f5c523eaf9f9063f6ef570d56273b4701ca7
GregPK <grzegorz.kaczorek@gmail.com>
2020-02-14 12:25:50 +0100

    Add update method to boat controller

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -4,7 +4,17 @@ class BoatsController < ApplicationController
   def index
     render json: Boat.all
   end
+  
   def show
     render json: Boat.find(params[:id])
+  end
+
+  def update
+    Boat.find(params[:id]).update!(boat_params)
+    render json: Boat.find(params[:id])
+  end
+
+  def boat_params
+    params.permit(:name, :size, :color, :description)
   end
 end
